5f0dd8ff408c7a7cb1851376aa12116213f16bbb
shreyas-londhe <shreyas_londhe@outlook.com>
2024-12-27 12:56:41 +0530

    fix: added debug to structs

--- core/src/structs.rs
+++ core/src/structs.rs
@@ -1,51 +1,51 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use serde::{Deserialize, Serialize};
 
-#[derive(BorshSerialize, BorshDeserialize)]
+#[derive(Debug, BorshSerialize, BorshDeserialize)]
 pub struct PublicKey {
     pub key: Vec<u8>,
     pub key_type: String,
 }
 
-#[derive(BorshSerialize, BorshDeserialize)]
+#[derive(Debug, BorshSerialize, BorshDeserialize)]
 pub struct DFA {
     pub fwd: Vec<u8>,
     pub bwd: Vec<u8>,
 }
 
-#[derive(BorshSerialize, BorshDeserialize)]
+#[derive(Debug, BorshSerialize, BorshDeserialize)]
 pub struct CompiledRegex {
     pub verify_re: DFA,
     pub capture_str: Option<String>,
 }
 
-#[derive(BorshSerialize, BorshDeserialize)]
+#[derive(Debug, BorshSerialize, BorshDeserialize)]
 pub struct RegexInfo {
     pub header_parts: Vec<CompiledRegex>,
     pub body_parts: Vec<CompiledRegex>,
 }
 
-#[derive(BorshSerialize, BorshDeserialize)]
+#[derive(Debug, BorshSerialize, BorshDeserialize)]
 pub struct Email {
     pub from_domain: String,
     pub raw_email: Vec<u8>,
     pub public_key: PublicKey,
 }
 
-#[derive(BorshSerialize, BorshDeserialize)]
+#[derive(Debug, BorshSerialize, BorshDeserialize)]
 pub struct EmailWithRegex {
     pub email: Email,
     pub regex_info: RegexInfo,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct EmailVerifierOutput {
     pub from_domain_hash: Vec<u8>,
     pub public_key_hash: Vec<u8>,
     pub verified: bool,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct EmailWithRegexVerifierOutput {
     pub email: EmailVerifierOutput,
     pub header_regex_verified: bool,
